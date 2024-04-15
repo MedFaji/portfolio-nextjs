7df3db6891461ba0303819b909e71092d31de48a
Med Faji <meedfaji@gmail.com>
2024-04-15 14:56:43 +0100

    Update Header component with cursor-pointer class

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
             to="contact"
             smooth={true}
             duration={800}
-            className="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500"
+            className="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500 cursor-pointer"
           >
             Hire me
           </Link>
