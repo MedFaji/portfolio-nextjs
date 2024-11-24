0acb311429abaad36cd35ebf96ccedc5d5ae3eef
medfaji <meedfaji@gmail.com>
2024-11-24 16:20:09 +0100

    edit next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Specify the output directory for exported static files
+  distDir: "out", // This sets the output directory to './out' instead of the default '.next'
+
+  // Optional settings
+  trailingSlash: true, // This ensures that URLs end with a trailing slash (e.g., '/about/')
+};
 
 export default nextConfig;
